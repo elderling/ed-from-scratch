3cc0ec5c6c6ae8c6159ca0948393aecb355f8445
Jonathan W. Taylor <elderling@gmail.com>
2021-10-15 15:48:19 -0500

    Add a bit of dynamic memory allocation

--- ed.c
+++ ed.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include <errno.h>
+#include <stddef.h>
 #define MAX_INPUT_LINE_LENGTH 88
 char command[MAX_INPUT_LINE_LENGTH];
 
@@ -11,14 +13,20 @@ typedef struct line {
     struct line * previous_line;
 } line;
 
-
+line * first_line;
+line * current_line;
 void main( void )
 {
 FILE * the_file;
 char input_line[255];
+char * new_line;
+first_line = NULL;
+current_line = NULL;
 the_file = fopen("ed.c", "r");
 while( fgets( input_line, 255, the_file) != NULL )  {
-puts(input_line);
+  new_line = (char *)malloc( 1 + strlen(input_line) );
+  strcpy(new_line, input_line);
+puts(new_line);
 }
 fclose(the_file);
     while(1) {
