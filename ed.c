9a68abd429fd1399b1d68b5e19f704543357079e
Jonathan W. Taylor <elderling@gmail.com>
2021-10-16 11:38:27 -0500

    astyle formatted

--- ed.c
+++ ed.c
@@ -17,18 +17,18 @@ line * first_line;
 line * current_line;
 void main( void )
 {
-FILE * the_file;
-char input_line[255];
-char * new_line;
-first_line = NULL;
-current_line = NULL;
-the_file = fopen("ed.c", "r");
-while( fgets( input_line, 255, the_file) != NULL )  {
-  new_line = (char *)malloc( 1 + strlen(input_line) );
-  strcpy(new_line, input_line);
-puts(new_line);
-}
-fclose(the_file);
+    FILE * the_file;
+    char input_line[255];
+    char * new_line;
+    first_line = NULL;
+    current_line = NULL;
+    the_file = fopen("ed.c", "r");
+    while( fgets( input_line, 255, the_file) != NULL )  {
+        new_line = (char *)malloc( 1 + strlen(input_line) );
+        strcpy(new_line, input_line);
+        puts(new_line);
+    }
+    fclose(the_file);
     while(1) {
         fgets(command, sizeof(command), stdin);
         printf("?\n");
