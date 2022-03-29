38df3fd3f78caf6281e7cdb262b5fbad2de5910d
Jonathan W. Taylor <elderling@gmail.com>
2022-03-29 11:19:08 -0500

    astyle --style=kr --unpad-paren

--- buffer.c
+++ buffer.c
@@ -10,174 +10,192 @@ struct line {
 
 struct buffer {
     line * head;
-		line * tail;
+    line * tail;
     unsigned int length;
 };
 
-buffer * new_buffer( void ) {
+buffer * new_buffer(void)
+{
 
-  buffer * nb;
+    buffer * nb;
 
-  nb = malloc(sizeof(buffer));
+    nb = malloc(sizeof(buffer));
 
-  nb->head = NULL;
-  nb->tail = NULL;
-	nb->length = 0;
+    nb->head = NULL;
+    nb->tail = NULL;
+    nb->length = 0;
 
-  return nb;
+    return nb;
 }
 
-line * buff_get_head( buffer * b ) {
-  return b->head;
+line * buff_get_head(buffer * b)
+{
+    return b->head;
 }
 
-line * buff_get_tail( buffer * b ) {
-  return b->tail;
+line * buff_get_tail(buffer * b)
+{
+    return b->tail;
 }
 
-unsigned int buff_get_length( buffer * b ) {
-	return b->length;
+unsigned int buff_get_length(buffer * b)
+{
+    return b->length;
 }
 
-buffer * buff_append_line( buffer * b, const char * s ) {
-	line * l;
+buffer * buff_append_line(buffer * b, const char * s)
+{
+    line * l;
 
-	l = new_line(s);
+    l = new_line(s);
 
-	if (b->head == NULL) {
-		b->head = l;
-		b->tail = l;
-		b->length = 1;
-		return b;
-	}
+    if(b->head == NULL) {
+        b->head = l;
+        b->tail = l;
+        b->length = 1;
+        return b;
+    }
 
-	if (b->head == b->tail) {
-		b->tail = l;
-		b->tail->prev = b->head;
-		b->head->next = l;
-		b->length++;
-		return b;
-  }
+    if(b->head == b->tail) {
+        b->tail = l;
+        b->tail->prev = b->head;
+        b->head->next = l;
+        b->length++;
+        return b;
+    }
 
-	l->prev = b->tail;
-	b->tail->next = l;
-	b->tail = l;
+    l->prev = b->tail;
+    b->tail->next = l;
+    b->tail = l;
 
-	return b;
+    return b;
 }
 
-line * new_line( const char * s ) {
+line * new_line(const char * s)
+{
 
-  char * ns;
-  line * nl;
+    char * ns;
+    line * nl;
 
-  nl = malloc(sizeof(line));
-  ns = malloc(strlen(s)+1);
-  
-  ns = strcpy(ns, s);
+    nl = malloc(sizeof(line));
+    ns = malloc(strlen(s)+1);
 
-  nl->prev = NULL;
-  nl->next = NULL;
-  nl->string = ns;
+    ns = strcpy(ns, s);
 
-  return nl;
+    nl->prev = NULL;
+    nl->next = NULL;
+    nl->string = ns;
+
+    return nl;
 }
 
-line * set_string( line * l, const char * s) {
-  char * ns;
-  free(l->string);
-  ns = malloc(strlen(s)+1);
-  ns = strcpy(ns, s);
-  l->string = ns;
+line * set_string(line * l, const char * s)
+{
+    char * ns;
+    free(l->string);
+    ns = malloc(strlen(s)+1);
+    ns = strcpy(ns, s);
+    l->string = ns;
 
-  return l;
+    return l;
 }
 
-char * get_string( line * l) {
-  return l->string;
+char * get_string(line * l)
+{
+    return l->string;
 }
 
-line * set_next( line * l, line * n) {
-	l->next = n;
-	n->prev = l;
-	return l;
+line * set_next(line * l, line * n)
+{
+    l->next = n;
+    n->prev = l;
+    return l;
 }
 
-line * get_next( line * l ) {
-	return l->next;
+line * get_next(line * l)
+{
+    return l->next;
 }
 
-line * set_prev( line * l, line * p) {
-	l->prev = p;
-	p->next = l;
-	return l;
+line * set_prev(line * l, line * p)
+{
+    l->prev = p;
+    p->next = l;
+    return l;
 }
 
-line * get_prev( line * l ) {
-	return l->prev;
+line * get_prev(line * l)
+{
+    return l->prev;
 }
 
-unsigned int count_children(line * l) {
-	unsigned int c = 0;
-	line * current;
-	current = l;
+unsigned int count_children(line * l)
+{
+    unsigned int c = 0;
+    line * current;
+    current = l;
 
-	while ( get_next(current) != NULL ) {
-		c++;
-		current = get_next(current);
-	}
+    while(get_next(current) != NULL) {
+        c++;
+        current = get_next(current);
+    }
 
-	return c;
+    return c;
 }
 
-unsigned int count_ancestors(line * l) {
-	unsigned int c = 0;
-	line * current;
-	current = l;
+unsigned int count_ancestors(line * l)
+{
+    unsigned int c = 0;
+    line * current;
+    current = l;
 
-	while ( get_prev(current) != NULL ) {
-		c++;
-		current = get_prev(current);
-	}
+    while(get_prev(current) != NULL) {
+        c++;
+        current = get_prev(current);
+    }
 
-	return c;
+    return c;
 }
 
-unsigned int list_size(line * l) {
-	return 1 + count_children(l) + count_ancestors(l);
+unsigned int list_size(line * l)
+{
+    return 1 + count_children(l) + count_ancestors(l);
 }
 
-void delete_line(line * l) {
-	if ( l->prev != NULL ) {
-		l->prev->next = l->next;
-	}
-	if ( l->next != NULL ) {
-		l->next->prev = l->prev;
-	}
+void delete_line(line * l)
+{
+    if(l->prev != NULL) {
+        l->prev->next = l->next;
+    }
+    if(l->next != NULL) {
+        l->next->prev = l->prev;
+    }
 
-	free(l->string);
-	free(l);
+    free(l->string);
+    free(l);
 
-	return;
+    return;
 }
 
-void insert_after (line * l, line * il) {
-	if ( l->next != NULL ) l->next->prev = il;
+void insert_after(line * l, line * il)
+{
+    if(l->next != NULL) l->next->prev = il;
 
-	il->prev = l;
-	il->next = l->next;
-	l->next = il;
-	
-	return;
+    il->prev = l;
+    il->next = l->next;
+    l->next = il;
+
+    return;
 }
 
-line * get_nth(line * l, unsigned int n) {
-	line * nth = l;
-	unsigned int i;
+line * get_nth(line * l, unsigned int n)
+{
+    line * nth = l;
+    unsigned int i;
 
-	for (i=0; i < n; i++) {
-		nth = nth->next;
-	}
+    for(i=0; i < n; i++) {
+        nth = nth->next;
+    }
 
-	return nth;
+    return nth;
 }
--- buffer_test.c
+++ buffer_test.c
@@ -12,23 +12,26 @@ void test_get_string(void);
 void test_set_next(void);
 void test_set_prev(void);
 void test_buff_get_head(void);
-void test_count_children( void );
-void test_count_ancestors( void );
-void test_list_size( void );
-void test_delete_line( void );
+void test_count_children(void);
+void test_count_ancestors(void);
+void test_list_size(void);
+void test_delete_line(void);
 void test_insert_after(void);
 void test_get_nth(void);
 void test_buff_append_line(void);
 
-void setUp(void) {
+void setUp(void)
+{
     return;
 }
 
-void tearDown(void) {
+void tearDown(void)
+{
     return;
 }
 
-void test_get_string(void) {
+void test_get_string(void)
+{
     line1 = new_line(str1);
 
     TEST_ASSERT_EQUAL_STRING(str1, get_string(line1));
@@ -36,7 +39,8 @@ void test_get_string(void) {
     return;
 }
 
-void test_buff_get_head(void) {
+void test_buff_get_head(void)
+{
     buffer1 = new_buffer();
 
     TEST_ASSERT_NULL(buff_get_head(buffer1));
@@ -44,213 +48,223 @@ void test_buff_get_head(void) {
     return;
 }
 
-void test_set_next(void) {
-	line * l;
-	line * n;
+void test_set_next(void)
+{
+    line * l;
+    line * n;
 
-	l = new_line("first");
-	n = new_line("second");
+    l = new_line("first");
+    n = new_line("second");
 
-	set_next(l,n);
+    set_next(l,n);
 
-	TEST_ASSERT_EQUAL_PTR(n, get_next(l));
+    TEST_ASSERT_EQUAL_PTR(n, get_next(l));
 
-	return;
+    return;
 }
 
-void test_set_prev(void) {
-	line * l;
-	line * p;
+void test_set_prev(void)
+{
+    line * l;
+    line * p;
 
-	l = new_line("first");
-	p = new_line("previous");
+    l = new_line("first");
+    p = new_line("previous");
 
-	set_prev(l,p);
+    set_prev(l,p);
 
-	TEST_ASSERT_EQUAL_PTR(p, get_prev(l));
+    TEST_ASSERT_EQUAL_PTR(p, get_prev(l));
 
-	return;
+    return;
 }
 
-void test_count_children( void ) {
-		line * head;
-		line * current;
-		char i;
-	  char * s;
-		s = malloc(2);
-		memset(s,0,2);
-		head = new_line("head");
-		TEST_ASSERT_EQUAL_UINT(0, count_children(head));
-		current = head;
+void test_count_children(void)
+{
+    line * head;
+    line * current;
+    char i;
+    char * s;
+    s = malloc(2);
+    memset(s,0,2);
+    head = new_line("head");
+    TEST_ASSERT_EQUAL_UINT(0, count_children(head));
+    current = head;
 
-		for (i = 0; i < 3; i++) {
-			sprintf(s, "%d", i);
-			set_next(current, new_line(s));
-			current = get_next(current);
-		}
+    for(i = 0; i < 3; i++) {
+        sprintf(s, "%d", i);
+        set_next(current, new_line(s));
+        current = get_next(current);
+    }
 
-		TEST_ASSERT_EQUAL_UINT(3, count_children(head));
+    TEST_ASSERT_EQUAL_UINT(3, count_children(head));
 
-		current = head;
+    current = head;
 
-		TEST_ASSERT_EQUAL_STRING("head", get_string(current));
-		current = get_next(current);
-		TEST_ASSERT_EQUAL_STRING("0", get_string(current));
-		current = get_next(current);
-		TEST_ASSERT_EQUAL_STRING("1", get_string(current));
+    TEST_ASSERT_EQUAL_STRING("head", get_string(current));
+    current = get_next(current);
+    TEST_ASSERT_EQUAL_STRING("0", get_string(current));
+    current = get_next(current);
+    TEST_ASSERT_EQUAL_STRING("1", get_string(current));
 
-		return;
+    return;
 }
 
-void test_count_ancestors( void ) {
-		line * head;
-		line * current;
-		char i;
-	  char * s;
-		s = malloc(2);
-		memset(s,0,2);
-		head = new_line("head");
-		TEST_ASSERT_EQUAL_UINT(0, count_ancestors(head));
-		current = head;
+void test_count_ancestors(void)
+{
+    line * head;
+    line * current;
+    char i;
+    char * s;
+    s = malloc(2);
+    memset(s,0,2);
+    head = new_line("head");
+    TEST_ASSERT_EQUAL_UINT(0, count_ancestors(head));
+    current = head;
 
-		for (i = 0; i < 3; i++) {
-			sprintf(s, "%d", i);
-			set_next(current, new_line(s));
-			current = get_next(current);
-		}
+    for(i = 0; i < 3; i++) {
+        sprintf(s, "%d", i);
+        set_next(current, new_line(s));
+        current = get_next(current);
+    }
 
-		TEST_ASSERT_EQUAL_UINT(3, count_ancestors(current));
-		TEST_ASSERT_EQUAL_STRING("2", get_string(current));
-		current = get_prev(current);
-		TEST_ASSERT_EQUAL_STRING("1", get_string(current));
-		current = get_prev(current);
-		TEST_ASSERT_EQUAL_STRING("0", get_string(current));
+    TEST_ASSERT_EQUAL_UINT(3, count_ancestors(current));
+    TEST_ASSERT_EQUAL_STRING("2", get_string(current));
+    current = get_prev(current);
+    TEST_ASSERT_EQUAL_STRING("1", get_string(current));
+    current = get_prev(current);
+    TEST_ASSERT_EQUAL_STRING("0", get_string(current));
 
-		return;
+    return;
 }
 
-void test_list_size( void ) {
-		line * head;
-		line * current;
-		char i;
-	  char * s;
-		s = malloc(2);
-		memset(s,0,2);
-		head = new_line("head");
-		TEST_ASSERT_EQUAL_UINT(0, count_ancestors(head));
-		current = head;
+void test_list_size(void)
+{
+    line * head;
+    line * current;
+    char i;
+    char * s;
+    s = malloc(2);
+    memset(s,0,2);
+    head = new_line("head");
+    TEST_ASSERT_EQUAL_UINT(0, count_ancestors(head));
+    current = head;
 
-		for (i = 0; i < 3; i++) {
-			sprintf(s, "%d", i);
-			set_next(current, new_line(s));
-			current = get_next(current);
-		}
+    for(i = 0; i < 3; i++) {
+        sprintf(s, "%d", i);
+        set_next(current, new_line(s));
+        current = get_next(current);
+    }
 
-		TEST_ASSERT_EQUAL_UINT(4, list_size(current));
-		current = get_prev(current);
-		TEST_ASSERT_EQUAL_UINT(4, list_size(current));
-		current = get_prev(current);
-		TEST_ASSERT_EQUAL_UINT(4, list_size(current));
-		TEST_ASSERT_EQUAL_UINT(4, list_size(head));
+    TEST_ASSERT_EQUAL_UINT(4, list_size(current));
+    current = get_prev(current);
+    TEST_ASSERT_EQUAL_UINT(4, list_size(current));
+    current = get_prev(current);
+    TEST_ASSERT_EQUAL_UINT(4, list_size(current));
+    TEST_ASSERT_EQUAL_UINT(4, list_size(head));
 
-		return;
+    return;
 }
 
-void test_delete_line( void ) {
-		line * head;
-		line * current;
-		char i;
-	  char * s;
-		s = malloc(2);
-		memset(s,0,2);
-		head = new_line("head");
-		current = head;
+void test_delete_line(void)
+{
+    line * head;
+    line * current;
+    char i;
+    char * s;
+    s = malloc(2);
+    memset(s,0,2);
+    head = new_line("head");
+    current = head;
 
-		for (i = 0; i < 3; i++) {
-			sprintf(s, "%d", i);
-			set_next(current, new_line(s));
-			current = get_next(current);
-		}
+    for(i = 0; i < 3; i++) {
+        sprintf(s, "%d", i);
+        set_next(current, new_line(s));
+        current = get_next(current);
+    }
 
-		current = get_next(head);
+    current = get_next(head);
 
-		delete_line(current);
+    delete_line(current);
 
-		TEST_ASSERT_EQUAL_UINT(3, list_size(head));
+    TEST_ASSERT_EQUAL_UINT(3, list_size(head));
 
-		return;
+    return;
 }
 
-void test_insert_after(void) {
-	char * s = "test";
-	unsigned char i;
-	line * head;
-	line * il;
-	line * current;
+void test_insert_after(void)
+{
+    char * s = "test";
+    unsigned char i;
+    line * head;
+    line * il;
+    line * current;
 
-	head = new_line(s);
-	current = head;
+    head = new_line(s);
+    current = head;
 
-	for ( i=0; i<3; i++) {
-		il = new_line(s);
-		insert_after(current, il);
-		current = il;
-	}
+    for(i=0; i<3; i++) {
+        il = new_line(s);
+        insert_after(current, il);
+        current = il;
+    }
 
-	TEST_ASSERT_EQUAL_UINT(4, list_size(head));
-	
-	return;
+    TEST_ASSERT_EQUAL_UINT(4, list_size(head));
+
+    return;
 }
 
-void test_get_nth(void) {
-		line * head;
-		line * current;
-		char i;
-	  char * s;
-		s = malloc(2);
-		memset(s,0,2);
-		head = new_line("head");
-		current = head;
+void test_get_nth(void)
+{
+    line * head;
+    line * current;
+    char i;
+    char * s;
+    s = malloc(2);
+    memset(s,0,2);
+    head = new_line("head");
+    current = head;
 
-		for (i = 0; i < 3; i++) {
-			sprintf(s, "%d", i);
-			set_next(current, new_line(s));
-			current = get_next(current);
-		}
+    for(i = 0; i < 3; i++) {
+        sprintf(s, "%d", i);
+        set_next(current, new_line(s));
+        current = get_next(current);
+    }
 
-	TEST_ASSERT_EQUAL_STRING(get_string(get_nth(head, 0)), "head");
-	TEST_ASSERT_EQUAL_STRING(get_string(get_nth(head, 3)), "2");
+    TEST_ASSERT_EQUAL_STRING(get_string(get_nth(head, 0)), "head");
+    TEST_ASSERT_EQUAL_STRING(get_string(get_nth(head, 3)), "2");
 
-	return;
+    return;
 }
 
-void test_buff_append_line(void) {
-	buffer * b;
-	b = new_buffer();
-	
-  buff_append_line(b, "first line");
-	TEST_ASSERT_EQUAL_UINT(1, buff_get_length(b));
-	TEST_ASSERT_EQUAL_STRING("first line",get_string(buff_get_head(b)));
-	buff_append_line(b, "second line");
-	TEST_ASSERT_NOT_EQUAL(buff_get_head(b), buff_get_tail(b));
-	buff_append_line(b, "third line");
-	TEST_ASSERT_EQUAL_UINT(2, count_ancestors(buff_get_tail(b)));
+void test_buff_append_line(void)
+{
+    buffer * b;
+    b = new_buffer();
+
+    buff_append_line(b, "first line");
+    TEST_ASSERT_EQUAL_UINT(1, buff_get_length(b));
+    TEST_ASSERT_EQUAL_STRING("first line",get_string(buff_get_head(b)));
+    buff_append_line(b, "second line");
+    TEST_ASSERT_NOT_EQUAL(buff_get_head(b), buff_get_tail(b));
+    buff_append_line(b, "third line");
+    TEST_ASSERT_EQUAL_UINT(2, count_ancestors(buff_get_tail(b)));
 }
 
-int main(void) {
+int main(void)
+{
     UNITY_BEGIN();
 
     RUN_TEST(test_get_string);
     RUN_TEST(test_buff_get_head);
     RUN_TEST(test_set_next);
     RUN_TEST(test_set_prev);
-		RUN_TEST(test_count_children);
-		RUN_TEST(test_count_ancestors);
-		RUN_TEST(test_list_size);
-		RUN_TEST(test_delete_line);
-		RUN_TEST(test_insert_after);
-		RUN_TEST(test_get_nth);
-		RUN_TEST(test_buff_append_line);
+    RUN_TEST(test_count_children);
+    RUN_TEST(test_count_ancestors);
+    RUN_TEST(test_list_size);
+    RUN_TEST(test_delete_line);
+    RUN_TEST(test_insert_after);
+    RUN_TEST(test_get_nth);
+    RUN_TEST(test_buff_append_line);
 
     return UNITY_END();
 }
