2a8a08f97995da319f4cac31f3306e74a650dcf1
Jonathan W. Taylor <elderling@gmail.com>
2022-04-23 10:33:33 -0500

    Add defines to exclude math.h & floats

--- buffer_test.sh
+++ buffer_test.sh
@@ -1,3 +1,3 @@
-gcc buffer.c buffer_test.c Unity/src/unity.c -o buffer_test
+gcc -D UNITY_EXCLUDE_MATH_H -D UNITY_EXCLUDE_FLOAT buffer.c buffer_test.c Unity/src/unity.c -o buffer_test
 ./buffer_test
 rm buffer_test
