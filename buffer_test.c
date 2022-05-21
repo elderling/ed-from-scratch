2ab8ef6d11e1cb12c55891057cbbd0e165d23da8
Jonathan W. Taylor <elderling@gmail.com>
2022-05-21 15:55:27 -0500

    Add tests for "constructors"

--- buffer_test.c
+++ buffer_test.c
@@ -4,6 +4,9 @@
 #include "buffer.h"
 #include "Unity/src/unity.h"
 
+void test_line_new( void );
+void test_buffer_new( void );
+
 void setUp(void)
 {
     return;
@@ -14,9 +17,33 @@ void tearDown(void)
     return;
 }
 
+void test_line_new( void )
+{
+  line * l;
+
+  l = line_new("test line new");
+
+  TEST_ASSERT_EQUAL_STRING("test line new", l->string);
+  TEST_ASSERT_NULL(l->next);
+  TEST_ASSERT_NULL(l->prev);
+}
+
+void test_buffer_new( void )
+{
+  buffer * b;
+
+  b = buffer_new();
+
+  TEST_ASSERT_NULL(b->head);
+  TEST_ASSERT_NULL(b->tail);
+}
+
 int main(void)
 {
     UNITY_BEGIN();
+
+    RUN_TEST(test_line_new);
+    RUN_TEST(test_buffer_new);
 
     return UNITY_END();
 }
